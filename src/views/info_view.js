71bd3ac72f815368655847ad5313f0e7000d1a1c
nimbus117 <mail@nimbus117.co.uk>
2018-09-12 14:48:32 +0100

    added render function

--- src/views/info_view.js
+++ src/views/info_view.js
@@ -7,7 +7,22 @@ const InfoView = function (element) {
 
 InfoView.prototype.bindEvent = function () {
   PubSub.subscribe('Countries:country-details', e => {
-    console.log(e.detail);
+    // console.log(e.detail);
+    this.render(e.detail);
+  })
+}
+
+InfoView.prototype.render = function (country) {
+  this.element.innerHTML = '';
+  createAndAppend('h2', country.name, this.element);
+  const flag = createAndAppend('img', '', this.element);
+  flag.src = country.flag;
+  createAndAppend('h3', 'Region:', this.element);
+  createAndAppend('p', country.region, this.element);
+  createAndAppend('h3', 'Languages:', this.element);
+  const list = createAndAppend('ul', '', this.element);
+  country.languages.forEach(language => {
+    createAndAppend('li', language.name, list);
   })
 }
 
